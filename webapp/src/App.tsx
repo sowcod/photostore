885169997dc36437d0d1b335feb4764bcdab95e8
Sohei Kotoh <771434+sowcod@users.noreply.github.com>
2022-10-28 21:41:48 +0900

    install MUI

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'
 
 function App() {
   return (
     <div>
-      Initial App
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon/>
+          </IconButton>
+          <Typography>
+            Initial App
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
